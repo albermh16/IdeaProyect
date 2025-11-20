027bd28c224c0d557900bb95d56287e6ab725d6f
Alberto Martinez <albermh16@gmail.com>
2025-11-20 13:40:49 +0100

    Revert "FoodExpressAPI con maven"
    This reverts commit f80c55cd10e2dce40b40b90c62ba9a9e48d92f99.

--- SpringBoot-Projects/FoodExpressAPI/src/main/java/es/daw/foodexpressapi/dto/AuthRequest.java
+++
@@ -1,9 +0,0 @@
-package es.daw.foodexpressapi.dto;
-
-import lombok.Data;
-
-@Data
-public class AuthRequest {
-    private String username;
-    private String password;
-}
--- SpringBoot-Projects/FoodExpressAPI/src/main/java/es/daw/foodexpressapi/repository/RestaurantRepository.java
+++
@@ -1,7 +0,0 @@
-package es.daw.foodexpressapi.repository;
-
-import es.daw.foodexpressapi.entity.Restaurant;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
-}
